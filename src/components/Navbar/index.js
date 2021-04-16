5e898a3e0a6da8d6f6675ef5c15bd87abe219d3e
idoyudha <idowidya.yudhatama@gmail.com>
2021-04-17 04:54:58 +0700

    Add github logo

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+import { Nav, NavLogo, NavbarContainer, MobileIcon, NavMenu, NavItem, NavLinks, GitLogo  } from './NavbarElements'
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">ido</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                        <FaGithubSquare />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="projects">Projects</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="resume">Resume</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <GitLogo to="github.com"><FaGithubSquare /></GitLogo>
+                    {/* <NavBtn>
+                        <NavBtnLink><FaGithubSquare /></NavBtnLink>
+                    </NavBtn> */}
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
